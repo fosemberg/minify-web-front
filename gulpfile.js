c0cd8ea3b8f87c9737b8576f21e8b215a2299bbf
fosemberg <fosemberg@gmail.com>
2021-10-20 12:56:27 +0300

    add copyFiles, compressHtml, compressCss, compressJs

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+const { src, dest, parallel, series } = require('gulp');
+
+const minifyHtml = require('gulp-htmlmin');
+const minifyHtmlInline = require('gulp-minify-inline');
+const minifyCss = require('gulp-clean-css');
+const minifyJs = require('gulp-minify');
+
+const pathToData = path.join('data');
+const pathToFiles = path.join(`${pathToData}${path.sep}**${path.sep}*`);
+const pathToHtml = path.join(`${pathToFiles}.html`);
+const pathToCss = path.join(`${pathToFiles}.css`);
+const pathToJs = path.join(`${pathToFiles}.js`);
+
+const pathToBuild = path.join('build');
+
+const compressHtml = () => src(pathToHtml)
+  .pipe(minifyHtml({ collapseWhitespace: true }))
+  .pipe(minifyHtmlInline())
+  .pipe(dest(pathToBuild));
+
+const compressCss = () => src(pathToCss)
+  .pipe(minifyCss())
+  .pipe(dest(pathToBuild));
+
+const compressJs = () => src(pathToJs)
+  .pipe(minifyJs({
+    ext: {
+      min: '.js'
+    },
+    noSource: true
+  }))
+  .pipe(dest(pathToBuild));
+
+const copyFiles = () => src(pathToFiles)
+  .pipe(dest(pathToBuild));
+
+exports.default = series(
+  copyFiles,
+  parallel(compressHtml, compressCss, compressJs)
+);
